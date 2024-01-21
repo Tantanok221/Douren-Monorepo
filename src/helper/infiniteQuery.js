9c4731d424d862e295267e056d28bedded678faa
Tan Kai <6572withspicy@gmail.com>
2024-01-21 18:14:04 +0800

    feat: Implemented search function that work with sort/filter tag

--- src/helper/infiniteQuery.js
+++ src/helper/infiniteQuery.js
@@ -1,24 +1,32 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { supabase } from "./supabase";
+import { useSearch } from "../hooks/useSearch";
+
+
+
+
+
 export function infiniteQuery(table, ascending, tagFilter) {
+  const search = useSearch((state) => state.search);
   return useInfiniteQuery({
-    queryKey: ["FF42", { table, ascending, tagFilter }],
+    queryKey: ["FF42" ,{ table, ascending  }, {tagFilter}],
     queryFn: async ({ pageParam }) => {
       let filterEmpty = tagFilter.length === 0;
       const { start, limit } = pageParam;
       let query = supabase.from("FF42").select("");
 
       if (!filterEmpty) {
-        const conditions = tagFilter.map(tag => `tag.ilike.%${tag}%`).join(',');
+        let conditions = tagFilter.map(tag => `tag.ilike.%${tag}%`)
+        conditions =  conditions.join(',')
         query = query.or(conditions);
       }
-
+      
       query = query.range(start, start + limit - 1).order(table, { ascending });
       const { data, error } = await query;
-      console.log(data);
       if (error) throw error;
       return data;
     },
+    enabled: search.length === 0,
     initialPageParam: { start: 1, limit: 20 },
     getNextPageParam: (lastPage, allPages, lastPageParam) => {
       if (lastPage.length === 0) {
--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -8,35 +8,37 @@ import SearchBox from "../../components/SearchBox/SearchBox.jsx";
 import SortSelect from "../../components/SortSelect/SortSelect.jsx";
 import styles from "./style.module.css";
 import { useSort } from "../../hooks/useSort.js";
-import { useDebounce } from "@uidotdev/usehooks";
 import { TagFilter } from "../../components/TagFilter/TagFilter.jsx";
 import { useTagFilter } from "../../hooks/useTagFilter.js";
 import { useSearch } from "../../hooks/useSearch.js";
 
 function MainLayout() {
-  const [posts, setPosts] = React.useState([]);
+  const [posts, setPosts] = React.useState(false);
   const search = useSearch((state) => state.search);
-  const debounceSearch = useDebounce(search, 500);
   const table = useSort((state) => state.table);
   const ascending = useSort((state) => state.ascending);
   const setAllFilter = useTagFilter((state) => state.setAllFilter);
   const allFilter = useTagFilter((state) => state.allFilter);
-  const tagFilter = useTagFilter((state) => state.tagFilter).flatMap((item) => item.tag);
-  const tagFilterList = useTagFilter((state) => state.tagFilter)
+  const tagFilter = useTagFilter((state) => state.tagFilter).flatMap(
+    (item) => item.tag
+  );
+  const tagFilterList = useTagFilter((state) => state.tagFilter);
   const removeAllTagFilter = useTagFilter((state) => state.removeAllTagFilter);
   useEffect(() => {
     setAllFilter();
   }, []);
   // Infinite Scroll
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status,
-  } = infiniteQuery(table, ascending,tagFilter);
+  
+    const {
+      data,
+      error,
+      fetchNextPage,
+      hasNextPage,
+      isFetching,
+      isFetchingNextPage,
+      status,
+    } = infiniteQuery(table, ascending, tagFilter);
+  console.log(tagFilter)
   const lastPostRef = React.useRef(null);
   const { ref, entry } = useIntersection({
     root: lastPostRef.current,
@@ -53,35 +55,47 @@ function MainLayout() {
   }, [data]);
   // Search Function Implementation
   useEffect(() => {
-    removeAllTagFilter()
-    if (debounceSearch !== "") {
-      let _data = async () => {
-        let _data = await supabase
-          .from("FF42")
-          .select("*")
-          .ilike("author_name", `%${debounceSearch}%`);
-  
-        return _data;
-      };
-      _data().then((result) => {
-        
-        setPosts(result?.data.flatMap((page) => page));
-      });
-    }
-    if (debounceSearch === "") {
-      setPosts(data?.pages.flatMap((page) => page));
-    }
-  }, [debounceSearch]);
-  
+    if (search.length > 0) {
+      setTimeout(async () => {
+        let query = supabase.from("FF42").select("");
 
-  if (!posts || !allFilter) {
+        if (!tagFilter.length === 0) {
+          const conditions = tagFilter
+            .map((tag) => `(tag.ilike.%${tag}%)`)
+            .join(",");
+          query = query.or(conditions);
+        }
+        query = query.filter("author_name", "ilike", `%${search}%`);
+
+        query = query.order(table, { ascending });
+        let { data, error } = await query;
+        console.log(tagFilter)
+        if (tagFilter.length !== 0) {
+          // console.log(data)
+          data = data.filter((item) => {
+            // console.log(item)
+            if(!item.tag) return false;
+            const tag = item.tag.split(",");
+            tag.pop() 
+            // console.log(tag);
+            return tagFilter.every((filter) => tag.includes(filter));
+          });
+        }
+        console.log(data)
+        setPosts(data);
+      }, 0);
+    }
+  }, [search,tagFilterList,table]);
+
+  if (search.length > 0 ? false :!posts || !allFilter) {
+    console.log(posts)
     return <div>Loading...</div>; // or some loading spinner
   }
   if (status === "error") {
     return <div>error</div>;
   }
-  if(!hasNextPage) {
-    console.log("No more data")
+  if (!hasNextPage) {
+    console.log("No more data");
   }
   const sx = classNames.bind(styles);
   return (
@@ -95,9 +109,11 @@ function MainLayout() {
         <TagFilter />
       </div>
       <div className={sx("ArtistContainer")}>
-        {(posts).map((item, index) => {
-          
-          if (index === posts.length - (tagFilterList.length === 0 ? 5 : 1 ) && search === "") {
+        {(posts ?? []).map((item, index) => {
+          if (
+            index === posts.length - (tagFilterList.length === 0 ? 5 : 1) &&
+            search === ""
+          ) {
             return (
               <ArtistCard
                 key={item.id + index + item + table + ascending}
