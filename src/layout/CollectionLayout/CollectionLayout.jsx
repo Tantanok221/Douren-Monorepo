974094cec25fe482cb2fb186231a6de961dccd48
Tan Kai <6572withspicy@gmail.com>
2024-01-23 16:35:02 +0800

    feat: Added sdebar to collection page

--- src/layout/CollectionLayout/CollectionLayout.jsx
+++ src/layout/CollectionLayout/CollectionLayout.jsx
@@ -4,7 +4,7 @@ import classNames from "classnames/bind";
 import { motion } from "framer-motion";
 import { useCollection } from "../../hooks/useCollection";
 import ArtistCard from "../../components/ArtistCard/ArtistCard";
-
+import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
 export const CollectionLayout = () => {
   const [posts, setPosts] = React.useState(false);
   const sx = classNames.bind(style);
@@ -12,25 +12,33 @@ export const CollectionLayout = () => {
   const collection = useCollection((state) => state.collection);
   useEffect(() => {
     initCollection();
-    setPosts(true)
+    setPosts(true);
   }, []);
-  console.log(collection)
-  if(!posts) return null
+  console.log(collection);
+  if (!posts) return null;
   return (
-    <motion.div className={sx("collectionLayout")}>
-      <div className={sx("headerContainer")}>
-        <div className={sx("title")}>我的收藏</div>
-        <div className={sx("subtitle")}>透過標簽功能來加入我的收藏</div>
+    <div classname={sx("Root")}>
+      <Sidebar />
+      <></>
+      <div className={sx("mainContainer")}>
+        <motion.div className={sx("collectionLayout")}>
+          <div className={sx("headerContainer")}>
+            <div className={sx("title")}>我的收藏</div>
+            <div className={sx("subtitle")}>透過標簽功能來加入我的收藏</div>
+          </div>
+          <div className={sx("artistContainer")}>
+            {collection.length !== 0 ? (
+              collection.map((item, index) => {
+                return <ArtistCard key={item.id} data={item} />;
+              })
+            ) : (
+              <div className={sx("emptyText")}>
+                你目前還沒有把任何攤位加入我的收藏！
+              </div>
+            )}
+          </div>
+        </motion.div>
       </div>
-      <div className={sx("artistContainer")}>
-        {
-        collection.length!==0 ?
-        (collection).map((item, index) => {
-          return <ArtistCard key={item.id} data={item} />;
-        })
-        : <div className={sx("emptyText")}>你目前還沒有把任何攤位加入我的收藏！</div>
-        }
-      </div>
-    </motion.div>
+    </div>
   );
 };
