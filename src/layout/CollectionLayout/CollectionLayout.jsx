db7e720b19c18b3835c571171a863744b22c4a07
Tan Kai <6572withspicy@gmail.com>
2024-01-23 11:52:48 +0800

    feat: Collection Page now have a simple header and render collection artist

--- src/layout/CollectionLayout/CollectionLayout.jsx
+++ src/layout/CollectionLayout/CollectionLayout.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./CollectionLayout.module.css";
 import classNames from "classnames/bind";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import { useCollection } from "../../hooks/useCollection";
+import ArtistCard from "../../components/ArtistCard/ArtistCard";
 
 export const CollectionLayout = () => {
+  const [posts, setPosts] = React.useState(false);
   const sx = classNames.bind(style);
-  return (<motion.div className={sx("CollectionLayout")}>CollectionLayout </motion.div>)
-}
+  const initCollection = useCollection((state) => state.initCollection);
+  const collection = useCollection((state) => state.collection);
+  useEffect(() => {
+    initCollection();
+    setPosts(true)
+  }, []);
+  console.log(collection)
+  if(!posts) return null
+  return (
+    <motion.div className={sx("collectionLayout")}>
+      <div className={sx("headerContainer")}>
+        <div className={sx("title")}>我的收藏</div>
+        <div className={sx("subtitle")}>透過標簽功能來加入我的收藏</div>
+      </div>
+      <div className={sx("artistContainer")}>
+        {
+        collection.length!==0 ?
+        (collection).map((item, index) => {
+          return <ArtistCard key={item.id} data={item} />;
+        })
+        : <div className={sx("emptyText")}>你目前還沒有把任何攤位加入我的收藏！</div>
+        }
+      </div>
+    </motion.div>
+  );
+};
