25500d52b8faa9a8d7a729485a5a483a22eb051c
Tan Kai <6572withspicy@gmail.com>
2024-01-23 22:43:26 +0800

    fix: swap icon

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { MdOutlineBookmarkBorder } from "react-icons/md";
 import { RiHome2Line } from "react-icons/ri";
 import { IconContext } from "react-icons";
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   const sx = classNames.bind(style);
@@ -14,11 +14,12 @@ export const Sidebar = () => {
       <IconContext.Provider value={{ color: "#AAAAAA", size: "1.5rem" }}>
         <div className={sx("linkContainer")}>
           <Link to={"/"} className={sx("linkButton")}>
-            <MdOutlineBookmarkBorder />
+            <RiHome2Line />
             FF42
           </Link>
           <Link to={"/collection"} className={sx("linkButton")}>
-            <RiHome2Line />
+            
+            <MdOutlineBookmarkBorder />
             我的收藏
           </Link>
         </div>
