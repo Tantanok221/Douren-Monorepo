db4fc7224e2e7dc4f9f9e66c9659f0ade3b9a8d3
Tan Kai <6572withspicy@gmail.com>
2024-01-25 11:15:22 +0800

    style: change name from HeaderContainer to TitleContainer

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -3,33 +3,26 @@ import styles from "./style.module.css";
 import { motion } from "framer-motion";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import classNames from "classnames/bind";
-
 import { useCollection } from "../../hooks/useCollection";
 import ArtistCardContext from "./ArtistCardContext";
 import ImageContainer from "./subcomponent/imageContainer";
-import HeaderContainer from "./subcomponent/HeaderContainer";
+import TitleContainer from "./subcomponent/TitleContainer";
 import BookmarkContainer from "./subcomponent/BookmarkContainer";
 import TagContainer from "./subcomponent/TagContainer";
 import DayContainer from "./subcomponent/DayContainer";
 import LinkContainer from "./subcomponent/LinkContainer";
-
-
-
 const ArtistCard = React.forwardRef(({ data, passRef }, ref) => {
   const collection = useCollection((state) => state.collection);
   console.log(collection);
-  const sx = classNames.bind(styles);
-
-  
+  const sx = classNames.bind(styles);  
   return (
     <ArtistCardContext.Provider value={data}>
       <motion.div ref={passRef} className={sx("artistCard")}>
         <motion.div className={sx("mainContainer")}>
           <ImageContainer />
-
           <div className={sx("rightContainer")}>
             <div className={sx("firstRow")}>
-              <HeaderContainer />
+              <TitleContainer />
               <BookmarkContainer />
             </div>
             <TagContainer />
--- src/components/ArtistCard/subcomponent/TitleContainer.jsx
+++ src/components/ArtistCard/subcomponent/TitleContainer.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useArtistCardContext } from "../ArtistCardContext";
-const HeaderContainer = () => {
+const TitleContainer = () => {
   const sx = classNames.bind(styles);
   const data = useArtistCardContext();
   return (
-    <div className={sx("headerContainer")}>
+    <div className={sx("titleContainer")}>
       <div className={sx("header")}>{data.author_name}</div>
       <div className={sx("subheader")}>{data.doujin_name}</div>
     </div>
   );
 };
 
-export default HeaderContainer;
+export default TitleContainer;
