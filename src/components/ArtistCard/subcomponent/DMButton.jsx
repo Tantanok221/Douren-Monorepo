33c04892ecfdc545ddc60ab8049f97760719e2ca
Tan Kai <6572withspicy@gmail.com>
2024-01-25 11:10:35 +0800

    refactor: DM dialog in to its button

---
+++ src/components/ArtistCard/subcomponent/DMButton.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "../style.module.css";
+import { useArtistCardContext } from "../ArtistCardContext";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import * as Dialog from "@radix-ui/react-dialog";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { LinkComponent } from "./LinkComponent";
+const DMButton = () => {
+  const sx = classNames.bind(styles);
+  const data = useArtistCardContext();
+  return (data.DM ? (
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <motion.div
+            whileHover={{
+              backgroundColor: "#4D4D4D",
+            }}
+            className={sx("linkButton")}
+          >
+            <LinkComponent data={{ category: "DM" }} />
+            商品項目
+          </motion.div>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay>
+            <div className={sx("dialogOverlay")}></div>
+          </Dialog.Overlay>
+          <Dialog.Content aria-describedby={undefined}>
+            <div className={sx("dialogContent")}>
+              <VisuallyHidden.Root asChild>
+                {/* <IconContext.Provider
+                      value={{ color: "#FFFFFF", size: "3rem" }}
+                    > */}
+                <Dialog.Close className={sx("dialogClose")}>
+                  {/* <IoClose></IoClose> */}
+                </Dialog.Close>
+                {/* </IconContext.Provider> */}
+              </VisuallyHidden.Root>
+              <VisuallyHidden.Root asChild>
+                <Dialog.Title />
+              </VisuallyHidden.Root>
+              <div className={sx("DMContainer")}>
+                <LazyLoadImage
+                  className={sx("image")}
+                  effect="blur"
+                  src={data.DM}
+                />
+              </div>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    ) : null)
+  
+};
+
+export default DMButton;
---
+++ src/components/ArtistCard/subcomponent/LinkContainer.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "../style.module.css";
+import { useArtistCardContext } from "../ArtistCardContext";
+import { IconContext } from "react-icons";
+import { LinkComponent } from "./LinkComponent";
+
+import { motion } from "framer-motion";
+import DMButton from "./DMButton";
+
+function processLink(links, names, category) {
+
+  
+
+  if (!links) {
+    return [];
+  }
+  let link = (links ?? "").split("\n");
+  let name = (names ?? "")?.split("\n");
+  let result = [];
+
+  link?.forEach((item, index) => {
+    result.push({
+      category: category ?? "",
+      link: item ?? "",
+      name: name[index] ?? "",
+    });
+  });
+  return result;
+}
+
+const LinkContainer = () => {
+  const sx = classNames.bind(styles);
+  const data = useArtistCardContext();
+  let link = processLink(data.Facebook_link, data.Facebook_name, "Facebook");
+  link = link.concat(
+    processLink(data.Instagram_link, data.Instagram_name, "Instagram")
+  );
+  link = link.concat(processLink(data.PIXIV_link, data.PIXIV_name, "Pixiv"));
+  link = link.concat(processLink(data.Twitch_link, data.Twitch_name, "Twitch"));
+  link = link.concat(
+    processLink(data.Twitter_link, data.Twitter_name, "Twitter")
+  );
+  link = link.concat(
+    processLink(data.Youtube_link, data.Youtube_name, "Youtube")
+  );
+  link = link.concat(processLink(data.Plurk_link, data.Plurk_name, "Plurk"));
+  link = link.concat(processLink(data.Baha_link, data.Baha_name, "Baha"));
+  link = link.concat(processLink(data.other_website, "官網", "Other"));
+
+  return (
+    <div className={sx("linkContainer")}>
+      <IconContext.Provider
+        value={{
+          verticalAlign: "middle",
+          color: "#CBC3C3",
+          size: "1.5rem",
+        }}
+      >
+        <DMButton/>
+        {link.map((item, index) => (
+          <motion.a
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={sx("linkButton")}
+            key={item + index}
+            whileHover={{
+              backgroundColor: "#4D4D4D",
+            }}
+          >
+            <div className={sx("linkIcon")}>
+              <LinkComponent key={index} data={item} />
+            </div>
+            {item.name}
+          </motion.a>
+        ))}
+      </IconContext.Provider>
+    </div>
+  );
+};
+
+export default LinkContainer;
