fc32bf965ba187970962a97ee9588f3007762378
Tan Kai <6572withspicy@gmail.com>
2024-01-25 12:08:02 +0800

    chore(dev): Update generate component without index

--- src/template/component/TemplateName.jsx
+++ src/template/component/TemplateName.jsx
@@ -3,7 +3,9 @@ import style from "./TemplateName.module.css";
 import classNames from "classnames/bind";
 import {motion} from "framer-motion"
 
-export const TemplateName = () => {
+const TemplateName = () => {
   const sx = classNames.bind(style);
   return (<motion.div className={sx("TemplateName")}>TemplateName </motion.div>)
 }
+
+export default TemplateName
