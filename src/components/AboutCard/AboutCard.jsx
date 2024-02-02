95437d295d69e72a40beaa4c11c530fc044b3f31
Tantanok221 <6572withspicy@gmail.com>
2024-02-02 17:45:16 +0800

    feat: Added basic html to aboutcard

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import style from "./AboutCard.module.css";
 import classNames from "classnames/bind";
-import {motion} from "framer-motion"
-import { BiArrowToTop } from "react-icons/bi";
+import { motion } from "framer-motion";
 import { IconContext } from "react-icons";
-const AboutCard = () => {
+const AboutCard = ({ author_data }) => {
   const sx = classNames.bind(style);
-  return (<motion.div
-    
-    className={sx("AboutCard")}
-  >
-     </motion.div>)
-}
+  return (
+    <motion.div className={sx("AboutCard")}>
+      <div className={sx("header")}>
+        <h1 className={sx("title")}>{author_data.title}</h1>
+        <h2 className={sx("subtitle")}>{author_data.subtitle}</h2>
+      </div>
+      <p className={sx("description")}>{author_data.description}</p>
+    </motion.div>
+  );
+};
 
-export default AboutCard
+export default AboutCard;
