c559c9eb50767b29522265a2248828cb4ec925d6
Tan Kai <6572withspicy@gmail.com>
2024-02-03 17:10:52 +0800

    feat: Added queryClient at Root

--- src/routes/Root/Root.jsx
+++ src/routes/Root/Root.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { Outlet } from "react-router-dom";
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import style from './root.module.css'
+import {useQueryClient} from '@tanstack/react-query'
 const Root = () => {
+  const queryClient = useQueryClient();
   const sx = classNames.bind(style);
   return (
     <div className={sx("Root")}>
