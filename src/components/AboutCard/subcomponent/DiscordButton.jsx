73c2c62a117480a276891b78c13c64ed33cd9929
Tan Kai <6572withspicy@gmail.com>
2024-02-03 13:20:13 +0800

    feat: Added Discord Buttonsubcomponents

---
+++ src/components/AboutCard/subcomponent/DiscordButton.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import LinkIcon from "../../LinkIcon/LinkIcon";
+import classNames from "classnames/bind";
+import style from "../AboutCard.module.css";
+import * as Toast from "@radix-ui/react-toast";
+
+
+const DiscordButton = ({ discord_name }) => {
+  const [open, setOpen] = React.useState(false);
+  const timerRef = React.useRef(0);
+
+  React.useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
+  const sx = classNames.bind(style);
+  return (
+    <Toast.Provider>
+      <button
+        className={sx("linkButton")}
+        value={discord_name}
+        onClick={(event) => {
+          navigator.clipboard.writeText(event.target.value);
+          setOpen(false);
+          window.clearTimeout(timerRef.current);
+          timerRef.current = window.setTimeout(() => {
+            setOpen(true);
+          }, 100);
+        }}
+      >
+        <LinkIcon data={{ category: "discord" }}></LinkIcon>
+        {discord_name}
+      </button>
+      <Toast.Root duration={1000} open={open} onOpenChange={setOpen} className={sx('toastRoot')} >
+        <Toast.Title className={sx('toastTitle')}>
+          Discord 名字已經複製到剪貼簿
+        </Toast.Title>
+      </Toast.Root>
+      <Toast.Viewport className={sx('toastViewport')} />
+    </Toast.Provider>
+  );
+};
+
+export default DiscordButton;
