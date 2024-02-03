bee68e06a6ccb41fdfc8b3fe4f3a4a3536e340d0
Tan Kai <6572withspicy@gmail.com>
2024-02-03 16:37:44 +0800

    feat: Added Join Our Discord Button

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,33 +1,59 @@
 import classNames from "classnames/bind";
 import styles from "./style.module.css";
 import { motion } from "framer-motion";
-import {useQuery,useQueryClient} from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../../helper/supabase";
 import AboutCard from "../../components/AboutCard/AboutCard";
+import LinkContainer from "../../components/LinkContainer/LinkContainer";
+import { IconContext } from "react-icons";
 function AboutUs() {
   const sx = classNames.bind(styles);
-  const {data,isError,status} = useQuery({queryKey: ["aboutUs"], queryFn: async () => {
-    const res = await supabase.from("author").select("*");
-    return res.data;
-  }})
-  
+  const { data, isError, status } = useQuery({
+    queryKey: ["aboutUs"],
+    queryFn: async () => {
+      const res = await supabase.from("author").select("*");
+      return res.data;
+    },
+  });
+  const DiscordGroup = [
+    {
+      name: "我們的DC",
+      link: "https://discord.gg/fwMe8RwZqb",
+      category: "Discord",
+    },
+  ];
   console.log(data);
-  if(status === "loading") return <div>loading</div>
+  if (status === "loading") return <div>loading</div>;
   return (
     <div className={sx("mainContainer")}>
       <div className={sx("header")}>關於我們</div>
-      
+
       <div className={sx("authorContainer")}>
-        {status === "success" ?
-        data.map((item,index) => {
-          return  <AboutCard key={index} author_data={item}/>
-        }) : <div>error</div>} 
+        {status === "success" ? (
+          data.map((item, index) => {
+            return <AboutCard key={index} author_data={item} />;
+          })
+        ) : (
+          <div>error</div>
+        )}
       </div>
       <div className={sx("dcContainer")}>
-        <div className={sx("text")}>資料出錯? 想換你的照片? 想要在這個網站貢獻些什麽?  有什麽想要的功能? 歡迎加入我們的DC 來討論</div>
-        <div className={sx("linkContainer")}>
-
+        <div className={sx("text")}>
+          資料出錯? 想換你的照片? 想要在這個網站貢獻些什麽? 有什麽想要的功能?
+          歡迎加入我們的DC 來討論
         </div>
+        <IconContext.Provider
+          value={{
+            verticalAlign: "middle",
+            color: "#CBC3C3",
+            size: "1.5rem",
+          }}
+        >
+
+        <div className={sx("discordButton")}>
+          <LinkContainer link={DiscordGroup} />
+        </div>
+        </IconContext.Provider>
       </div>
     </div>
   );
