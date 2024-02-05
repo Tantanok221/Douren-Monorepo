88cb21e8ea9198b0a516a2e10ca80e4d07315725
Tan Kai <6572withspicy@gmail.com>
2024-02-05 11:03:26 +0800

    feat: Added Responsive to image Container

--- src/components/ArtistCard/subcomponent/ImageContainer.jsx
+++ src/components/ArtistCard/subcomponent/ImageContainer.jsx
@@ -4,14 +4,28 @@ import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useArtistCardContext } from "../ArtistCardContext";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useMediaQuery } from '@mantine/hooks';
 
 const ImageContainer = () => {
   const sx = classNames.bind(styles);
   const data = useArtistCardContext()
+  let width = "500px"
+  const tabSize = useMediaQuery('(min-width: 1300px)');
   
+  const phoneSize = useMediaQuery('(max-width: 590px)');
+  const smallPhoneSize = useMediaQuery('(max-width: 445px)');
+  if(tabSize) {
+    width = "48rem"
+  }
+  if(phoneSize) {
+    width = "35rem"
+  }
+  if(smallPhoneSize){
+    width = "25rem"
+  }
   return (
     <div className={sx("imageContainer")}>
-      <LazyLoadImage width="500px" alt={data.author_name + " pictures"} className={sx("image")} effect="blur" src={data.photo} />
+      <LazyLoadImage width={width} alt={data.author_name + " pictures"} className={sx("image")} effect="blur" src={data.photo} />
     </div>
   );
 };
