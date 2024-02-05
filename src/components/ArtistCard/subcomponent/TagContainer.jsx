c8b4a47a0e84945507253fb7798cd65e5ba158dd
Tan Kai <6572withspicy@gmail.com>
2024-02-05 16:20:50 +0800

    feat: Added remove tag behaviour and improve accessiblity

--- src/components/ArtistCard/subcomponent/TagContainer.jsx
+++ src/components/ArtistCard/subcomponent/TagContainer.jsx
@@ -3,8 +3,7 @@ import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useArtistCardContext } from "../ArtistCardContext";
 import { useTagFilter } from "../../../hooks/useTagFilter";
-import { TagFilter } from "../../TagFilter/TagFilter";
-
+import {motion} from "framer-motion";
 const TagContainer = () => {
   const sx = classNames.bind(styles);
   const data = useArtistCardContext();
@@ -12,9 +11,12 @@ const TagContainer = () => {
   const allTag = (data.tag ?? "").split(",");
   const tagFilter = useTagFilter((state) => state.tagFilter);
   const addTagFilter = useTagFilter((state) => state.addTagFilter);
+  const removeTagFilter = useTagFilter((state) => state.removeTagFilter)
   function handleClick(val) {
     if(tagFilter.filter((item) => item === val).length === 0) {
       addTagFilter(val);
+    }else {
+      removeTagFilter(val)
     }
   }
 
@@ -31,14 +33,15 @@ const TagContainer = () => {
         ? renderTag.map((val, index) => {
           let active = tagFilter.filter((item) => item === val).length !== 0
             return (
-              <button
+              <motion.button
                 key={index + val.tag}
                 onClick={() => handleClick(val)}
                 className={sx("tagItem")}
+                whileHover={{scale: 1.1}}
               >
                 <div className={sx("tagDescription",{activeTagCount: active} )}>{val.tag}</div>
                 <div className={sx("tagCount",{activeTagDescription: active})}>{val.count}</div>
-              </button>
+              </motion.button>
             );
           })
         : null}
