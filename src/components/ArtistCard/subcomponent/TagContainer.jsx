7b49e9ac12a1acd74310b44099a2901feabc0ac7
Tan Kai <6572withspicy@gmail.com>
2024-02-20 08:46:31 +0800

    fix: TagFilter Checkbox will not reflect when use click on tagContainer

--- src/components/ArtistCard/subcomponent/TagContainer.jsx
+++ src/components/ArtistCard/subcomponent/TagContainer.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useArtistCardContext } from "../ArtistCardContext";
 import { useTagFilter } from "../../../hooks/useTagFilter";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 const TagContainer = () => {
   const sx = classNames.bind(styles);
   const data = useArtistCardContext();
@@ -11,12 +11,19 @@ const TagContainer = () => {
   const allTag = (data.tag ?? "").split(",");
   const tagFilter = useTagFilter((state) => state.tagFilter);
   const addTagFilter = useTagFilter((state) => state.addTagFilter);
-  const removeTagFilter = useTagFilter((state) => state.removeTagFilter)
+  const removeTagFilter = useTagFilter((state) => state.removeTagFilter);
+  const checked = useTagFilter((state) => state.checked);
+  const setChecked = useTagFilter((state) => state.setChecked);
+
   function handleClick(val) {
-    if(tagFilter.filter((item) => item === val).length === 0) {
+    console.log(val)
+    
+    if (tagFilter.filter((item) => item === val).length === 0) {
       addTagFilter(val);
-    }else {
-      removeTagFilter(val)
+      setChecked(val.index - 1,true);
+    } else {
+      removeTagFilter(val);
+      setChecked(val.index - 1,false);
     }
   }
 
@@ -26,21 +33,30 @@ const TagContainer = () => {
     renderTag[index] = getTag(item);
   });
   renderTag = renderTag.flatMap((value) => value);
-
+  // console.log(renderTag[0])
+  console.log(tagFilter);
   return (
     <div className={sx("tagContainer")}>
       {data.tag
         ? renderTag.map((val, index) => {
-          let active = tagFilter.filter((item) => item === val).length !== 0
+            let active = tagFilter.filter((item) => item === val).length !== 0;
             return (
               <motion.button
                 key={index + val.tag}
                 onClick={() => handleClick(val)}
                 className={sx("tagItem")}
-                whileHover={{scale: 1.1}}
+                whileHover={{ scale: 1.1 }}
               >
-                <div className={sx("tagDescription",{activeTagCount: active} )}>{val.tag}</div>
-                <div className={sx("tagCount",{activeTagDescription: active})}>{val.count}</div>
+                <div
+                  className={sx("tagDescription", { activeTagCount: active })}
+                >
+                  {val.tag}
+                </div>
+                <div
+                  className={sx("tagCount", { activeTagDescription: active })}
+                >
+                  {val.count}
+                </div>
               </motion.button>
             );
           })
