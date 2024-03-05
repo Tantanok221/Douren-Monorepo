2a98dab4da42632186ad22fe35516b09dca6779b
Tan Kai <6572withspicy@gmail.com>
2024-03-05 15:41:19 +0800

    fix: ts build error

--- src/animate/Animate.tsx
+++ src/animate/Animate.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-const Animate = (OgComponent) => {
+const Animate = (OgComponent: React.FC) => {
   return () => (
     <>
       <OgComponent />
