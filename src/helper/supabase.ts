a139ef46edb082cfc9e51760bc2f3fc9170f2d53
Tan Kai <6572withspicy@gmail.com>
2024-03-14 15:38:25 +0800

    feat: remove supabase generated types

--- src/helper/supabase.ts
+++ src/helper/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { Database } from "../../types/supabase"
-export const supabase = createClient<Database>(import.meta.env.VITE_LINK, import.meta.env.VITE_PUBLIC_KEY )
+export const supabase = createClient(import.meta.env.VITE_LINK, import.meta.env.VITE_PUBLIC_KEY )
 
 
