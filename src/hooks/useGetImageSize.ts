f79ddecb8c962c4b24bda6d4138e9af5be685db4
Tan Kai <6572withspicy@gmail.com>
2024-03-18 10:33:50 +0800

    feat: scale down picture size

--- src/hooks/useGetImageSize.ts
+++ src/hooks/useGetImageSize.ts
@@ -1,14 +1,10 @@
 import { useMediaQuery } from '@mantine/hooks';
 
 export function useGetImageSize() {
-  let width = "500px"
-  const tabSize = useMediaQuery('(min-width: 1340px)');
-  
+  let width = "40rem"  
   const phoneSize = useMediaQuery('(max-width: 590px)');
   const smallPhoneSize = useMediaQuery('(max-width: 445px)');
-  if(tabSize) {
-    width = "48rem"
-  }
+
   if(phoneSize) {
     width = "35rem"
   }
