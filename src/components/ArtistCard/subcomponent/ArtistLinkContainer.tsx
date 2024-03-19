edbd7d30de03b481dbe42661bd73a3310bef0172
Tan Kai <6572withspicy@gmail.com>
2024-03-19 13:35:51 +0800

    feat: ArtistLinkContainer now work with artist data types

--- src/components/ArtistCard/subcomponent/ArtistLinkContainer.tsx
+++ src/components/ArtistCard/subcomponent/ArtistLinkContainer.tsx
@@ -7,6 +7,7 @@ import DMButton from "./DMButton";
 import { LinkResult, processLink } from "../../../helper/processLink";
 import LinkContainer from "../../LinkContainer/LinkContainer.tsx";
 import { useFFContext } from "../FFContext.ts";
+import { useArtistCardContext } from "../ArtistCardContext.ts";
 
 interface props {
   children?: React.ReactNode;
@@ -15,30 +16,31 @@ interface props {
 const ArtistLinkContainer = ({ children }: props) => {
   const sx = classNames.bind(styles);
   const FFData = useFFContext();
-
+  const ArtistData = useArtistCardContext();
   let link: LinkResult[] = [];
   if (FFData) {
     link = processLink(FFData.Facebook_link, FFData.Facebook_name, "Facebook");
-    link = link.concat(
-      processLink(FFData.Instagram_link, FFData.Instagram_name, "Instagram")
-    );
-    link = link.concat(
-      processLink(FFData.Pixiv_link, FFData.Pixiv_name, "Pixiv")
-    );
-    link = link.concat(
-      processLink(FFData.Twitch_link, FFData.Twitch_name, "Twitch")
-    );
-    link = link.concat(
-      processLink(FFData.Twitter_link, FFData.Twitter_name, "Twitter")
-    );
-    link = link.concat(
-      processLink(FFData.Youtube_link, FFData.Youtube_name, "Youtube")
-    );
-    link = link.concat(
-      processLink(FFData.Plurk_link, FFData.Plurk_name, "Plurk")
-    );
+    link = link.concat(processLink(FFData.Instagram_link, FFData.Instagram_name, "Instagram"));
+    link = link.concat(processLink(FFData.Pixiv_link, FFData.Pixiv_name, "Pixiv"));
+    link = link.concat(processLink(FFData.Twitch_link, FFData.Twitch_name, "Twitch"));
+    link = link.concat(processLink(FFData.Twitter_link, FFData.Twitter_name, "Twitter"));
+    link = link.concat(processLink(FFData.Youtube_link, FFData.Youtube_name, "Youtube"));
+    link = link.concat(processLink(FFData.Plurk_link, FFData.Plurk_name, "Plurk"));
     link = link.concat(processLink(FFData.Baha_link, FFData.Baha_name, "Baha"));
     link = link.concat(processLink(FFData.Official_link, "官網", "Other"));
+  }
+  if(ArtistData){
+    link = processLink(ArtistData.Facebook_link, ArtistData.Author, "Facebook");
+    link = link.concat(processLink(ArtistData.Instagram_link, ArtistData.Author, "Instagram"));
+    link = link.concat(processLink(ArtistData.Pixiv_link, ArtistData.Author, "Pixiv"));
+    link = link.concat(processLink(ArtistData.Twitch_link, ArtistData.Author, "Twitch"));
+    link = link.concat(processLink(ArtistData.Twitter_link, ArtistData.Author, "Twitter"));
+    link = link.concat(processLink(ArtistData.Youtube_link, ArtistData.Author, "Youtube"));
+    link = link.concat(processLink(ArtistData.Plurk_link, ArtistData.Author, "Plurk"));
+    link = link.concat(processLink(ArtistData.Baha_link, ArtistData.Author, "Baha"));
+    link = link.concat(processLink(ArtistData.Official_link, "官網", "Other"))
+    link = link.concat(processLink(ArtistData.Store_link, "商店", "Store"))
+    link = link.concat(processLink(ArtistData.Myacg_link, "MYACG", "Other"))
   }
 
   return (
