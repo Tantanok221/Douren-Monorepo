e78b23c196be36438772552654cf4d3dc969ed03
Tan Kai <6572withspicy@gmail.com>
2024-03-19 14:11:36 +0800

    feat: undefined value type supports

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -4,17 +4,18 @@ import { FF } from '../../types/FF';
 
 interface Collection {
   collection: FF[]
-  addCollection : (data:FF) => void
-  removeCollection : (data:FF) => void
+  addCollection : (data:FF| undefined) => void
+  removeCollection : (data:FF| undefined ) => void
   initCollection: () => void
   updateLocalStorage: () => void
-  checkAvailable: (data:FF) => boolean
+  checkAvailable: (data:FF| undefined) => boolean
 }
 
 
 export const useCollection = create<Collection>()((set, get) => ({
   collection: [],
   addCollection: (data) => {
+    if(!data) return 
     set((state) =>
       produce(state, (draftState) => {
         draftState.collection.push(data);
@@ -22,6 +23,7 @@ export const useCollection = create<Collection>()((set, get) => ({
     );
   },
   removeCollection: (data) => {
+    if(!data) return 
     set((state) => ({
       collection: state.collection.filter((val) => val.Booth_name !== data.Booth_name),
     }));
@@ -44,6 +46,7 @@ export const useCollection = create<Collection>()((set, get) => ({
     localStorage.setItem("FF42-Collection", JSON.stringify(get().collection));
   },
   checkAvailable: (data) => {
+    if(!data) return false
     const collectionName = get().collection.flatMap((val) => val.Booth_name);
     const dataName = data.Booth_name;
     return collectionName.filter((val) => val === dataName).length != 0;
