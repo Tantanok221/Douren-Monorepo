f467337a9878845aab26bf5705f6c37f3bbe72b0
Tan Kai <6572withspicy@gmail.com>
2024-03-19 14:57:52 +0800

    feat: Added ArtistTypes support for titleContainer

--- src/components/ArtistCard/subcomponent/TitleContainer.tsx
+++ src/components/ArtistCard/subcomponent/TitleContainer.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useFFContext } from "../FFContext";
+import { useArtistCardContext } from "../ArtistCardContext";
 const TitleContainer = () => {
   const sx = classNames.bind(styles);
   const data = useFFContext();
+  const artistData = useArtistCardContext();
+  const title = data ? data.Booth_name : artistData?.Author
+  const subtitle = data ? data.Author : artistData?.Introduction
   return (
     <div className={sx("titleContainer")}>
-      <div className={sx("header")}>{data.Booth_name}</div>
-      <div className={sx("subheader")}>{data.Author}</div>
+      <div className={sx("header")}>{title}</div>
+      <div className={sx("subheader")}>{subtitle}</div>
     </div>
   );
 };
