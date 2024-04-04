5ebb3625791b992a43bb9bc4064dd0b3dc3b9620
Tan Kai <6572withspicy@gmail.com>
2024-04-04 08:42:07 +0800

    feat: Added LazyImage components

--- src/components/ArtistCard/subcomponent/ImageContainer.tsx
+++ src/components/ArtistCard/subcomponent/ImageContainer.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useFFContext } from "../FFContext";
-import "react-lazy-load-image-component/src/effects/blur.css";
 import { useGetImageSize } from "../../../hooks/useGetImageSize";
 import { useArtistCardContext } from "../ArtistCardContext";
+import LazyImage from "../../LazyImage/LazyImage";
 
 interface Props {
-  followContainerSize?: boolean;
-
 }
-const ImageContainer = ({followContainerSize}:Props) => {
+const ImageContainer = ({}:Props) => {
   const sx = classNames.bind(styles);
   const data = useFFContext();
   const artistData = useArtistCardContext();
@@ -19,13 +16,7 @@ const ImageContainer = ({followContainerSize}:Props) => {
   let photo = data ? data.Photo : artistData?.Photo;
   return (
     <div className={sx("imageContainer")}>
-      <LazyLoadImage
-        width={followContainerSize ? "" : width}
-        alt={data?.Booth_name  + " pictures"}
-        className={sx("image")}
-        effect="blur"
-        src={photo}
-      />
+      <LazyImage alt={artistData?.Author} photo={photo} width={width}></LazyImage>
     </div>
   );
 };
