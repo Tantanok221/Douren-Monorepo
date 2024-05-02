9e982b411444346f4152ddb326300b9099620fa3
Tan Kai <6572withspicy@gmail.com>
2024-05-02 12:40:06 +0800

    feat: Change FF42 to tsx

--- src/pages/FF42/FF42.tsx
+++ src/pages/FF42/FF42.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../../helper/supabase.ts";
-import { infiniteQuery } from "../../helper/infiniteQuery.ts";
+import { FF42Query, infiniteQuery } from "../../helper/FF42Query.ts";
 import ArtistCard from "../../components/ArtistCard/ArtistCard.tsx";
 import classNames from "classnames/bind";
 import SearchBox from "../../components/SearchBox/SearchBox.tsx";
@@ -18,22 +18,21 @@ import Animate from "../../animate/Animate.tsx";
 import { useNextPageAvailable } from "../../hooks/useNextPageAvailable.ts";
 import ImageContainer from "../../components/ArtistCard/subcomponent/ImageContainer.tsx";
 import TagContainer from "../../components/ArtistCard/subcomponent/ArtistTagContainer.tsx";
-import DayContainer from "../../components/ArtistCard/subcomponent/DayContainer";
-import ArtistLinkContainer from "../../components/ArtistCard/subcomponent/ArtistLinkContainer";
+import DayContainer from "../../components/ArtistCard/subcomponent/DayContainer.tsx";
+import ArtistLinkContainer from "../../components/ArtistCard/subcomponent/ArtistLinkContainer.tsx";
 import HeaderContainer from "../../components/ArtistCard/subcomponent/HeaderContainer.tsx";
 import RightContainer from "../../components/ArtistCard/subcomponent/RightContainer.tsx";
 import DMButton from "../../components/ArtistCard/subcomponent/ArtistDMButton.tsx";
+import { FF } from "../../types/FF.ts";
 
 function FF42() {
   const FETCH_COUNT = 40;
-  const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);
   const [start, setStart] = useState(1);
   const [end, setEnd] = useState(FETCH_COUNT);
   const nextPageAvailable = useNextPageAvailable(
     (state) => state.nextPageAvailable
   );
-  const search = useSearch((state) => state.search);
   const table = useSort((state) => state.table);
   const ascending = useSort((state) => state.ascending);
   const setAllFilter = useTagFilter((state) => state.setAllFilter);
@@ -41,6 +40,7 @@ function FF42() {
   const tagFilter = useTagFilter((state) => state.tagFilter).flatMap(
     (item) => item.tag
   );
+  const search = useSearch((state) => state.search);
   const resetSearch = useSearch((state) => state.resetSearch);
   const tagFilterList = useTagFilter((state) => state.tagFilter);
   const initCollection = useCollection((state) => state.initCollection);
@@ -56,59 +56,20 @@ function FF42() {
   window.scrollTo(0, position);
   useEffect(() => {
     setAllFilter();
-    resetSearch();
     initNextPageAvailable();
     initCollection();
   }, []);
   // Infinite Scroll
-  const { data, error, isFetching, status } = infiniteQuery(
+  const { data, error, isFetching, status } = FF42Query(
     start,
     end,
     table,
     ascending,
     tagFilter
   );
+  
 
-  useEffect(() => {
-    if (data?.length != 0) setPosts(data);
-  }, [data]);
-  // Search Function Implementation
-  // !To do list: search tag filter broken
-  useEffect(() => {
-    if (search.length > 0) {
-      setTimeout(async () => {
-        let query = supabase.from("FF42").select("*");
 
-        if (tagFilter.length !== 0) {
-          const conditions = tagFilter
-            .map((tag) => `(Tag.ilike.%${tag}%)`)
-            .join(",");
-          query = query.or(conditions);
-        }
-        query = query.filter("Booth_name", "ilike", `%${search}%`);
-
-        query = query.order(table, { ascending });
-        let { data, error } = await query;
-        console.log(error)
-        if (tagFilter.length !== 0 && data) {
-          data = data.filter((item) => {
-            if (!item.Tag) return false;
-              const tag = item.Tag.split(",");
-              tag.pop();
-              // console.log(tag);
-              return tagFilter.every((filter) => tag.includes(filter));
-            });
-          }
-        setPosts(data);
-      }, 0);
-    }
-  }, [search, tagFilterList, table]);
-  if (isFetching) {
-    return <div>Fetching...</div>;
-  }
-  if (search.length > 0 ? false : !posts || !allFilter) {
-    return <div>Loading...</div>; // or some loading spinner
-  }
   if (status === "error") {
     return <div>error</div>;
   }
@@ -129,7 +90,7 @@ function FF42() {
         <TagFilter />
       </div>
       <div className={sx("ArtistContainer")}>
-        {(posts ?? []).map((item, index) => {
+        {(data ?? []).map((item, index) => {
           return (
             <ArtistCard key={`${item.uuid}`} data={item}>
               <ImageContainer />
