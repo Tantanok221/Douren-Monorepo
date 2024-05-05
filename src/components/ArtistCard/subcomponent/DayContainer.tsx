dc754937f3d40ccec73f78e27cc3c5e9edf1045b
Tan Kai <6572withspicy@gmail.com>
2024-05-05 10:43:23 +0800

    feat: DayContainer now can use eventData

--- src/components/ArtistCard/subcomponent/DayContainer.tsx
+++ src/components/ArtistCard/subcomponent/DayContainer.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useFFContext } from "../FFContext";
+import { useEventDataContext } from "../EventDataContext";
 const DayContainer = () => {
   const sx = classNames.bind(styles);
   const data = useFFContext();
-  const boothLocation = [
-    data?.DAY01_location,
-    data?.DAY02_location,
-    data?.DAY03_location,
-  ];
+  const eventData = useEventDataContext();
+  let boothLocation: string[] = [];
+  if (data?.DAY01_location) {
+    const boothLocation = [
+      data?.DAY01_location,
+      data?.DAY02_location,
+      data?.DAY03_location,
+    ];
+  } else if (eventData?.Location) {
+    boothLocation = eventData?.Location?.split(",");
+  }
+
   return (
     <div className={sx("dayContainer")}>
       {[1, 2, 3].map((day, index) => {
