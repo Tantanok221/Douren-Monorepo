32ecb6519d06d9dccbc19a341a70de2b57a21352
Tan Kai <6572withspicy@gmail.com>
2024-05-05 11:05:26 +0800

    feat: TitleContainer now can use eventDataType

--- src/components/ArtistCard/subcomponent/TitleContainer.tsx
+++ src/components/ArtistCard/subcomponent/TitleContainer.tsx
@@ -3,6 +3,8 @@ import classNames from "classnames/bind";
 import styles from "../style.module.css";
 import { useFFContext } from "../FFContext";
 import { useArtistCardContext } from "../ArtistCardContext";
+import { useEventDataContext } from "../EventDataContext";
+import { ArtistTypes } from "../../../types/Artist";
 
 interface Props {
   subtitleDisabled?: boolean;
@@ -11,9 +13,14 @@ interface Props {
 const TitleContainer = ({ subtitleDisabled }: Props) => {
   const sx = classNames.bind(styles);
   const data = useFFContext();
-  const artistData = useArtistCardContext();
-  const title = data ? data.Booth_name : artistData?.Author;
-  const subtitle = data ? data.Author : artistData?.Introduction;
+  let artistData = useArtistCardContext();
+  const eventData = useEventDataContext();
+  if (!artistData && !data) {
+    artistData = eventData?.Author_Main as ArtistTypes;
+  }
+
+  const subtitle = data ? data.Author : artistData?.Author;
+  const title = data ? data.Booth_name : eventData?.Booth_name;
   return (
     <div className={sx("titleContainer")}>
       <div className={sx("header")}>{title}</div>
