1955273442dd5b1531415cbac2a8df064ec57ba4
Tantanok221 <6572withspicy@gmail.com>
2024-05-06 08:48:30 +0800

    feat: Added sort,search and next page functionability

--- src/hooks/useEventIDQuery.ts
+++ src/hooks/useEventIDQuery.ts
@@ -23,6 +23,8 @@ export function useEventIDQuery(
   return useQuery({
     queryKey: [eventId],
     queryFn: async ():Promise<ArtistEventType[] | null> => {
+      const filterEmpty = tagFilter.length === 0;
+
       let query = supabase.from('Event_DM').select(`
       Location,
       Booth_name,
@@ -31,7 +33,19 @@ export function useEventIDQuery(
         *
       )
       `).eq('Event_id',eventId)
+      if (!filterEmpty) {
+        tagFilter.forEach((tag) => {
+          query = query.filter("Tag", "ilike", `%${tag}%`);
+        });
+      }
+      if (search.length > 0) {
+        query = query.filter("Booth_name", "ilike", `%${search}%`);
+      }
+      query = query.order(table, { ascending }).range(start, end + 1);
       const { data,error} = await query 
+      if ((data?.length ?? 0) < end - start + 1) {
+        setNextPageAvailable(false);
+      }
       console.log(data)
       if(error){
         throw error
