71648834da50339b9dc78870dd77ef3337640564
Tan Kai <6572withspicy@gmail.com>
2024-05-30 22:42:51 +0800

    feat: Change from localStorage hook to useReducer

--- src/context/CollectionContext/CollectionContext.tsx
+++ src/context/CollectionContext/CollectionContext.tsx
@@ -1,11 +1,17 @@
 import { createContext } from "react";
 import { useLocalStorage } from "@mantine/hooks";
 import { ArtistEventType } from "../../types/Artist";
+import { useImmerReducer } from "use-immer";
 
 
+export interface ActionType {
+  action: 'add' | 'remove'
+  keys: string 
+  data: ArtistEventType
+}
 interface contextProps {
   collection: ArtistEventType[];
-  setCollection: React.Dispatch<React.SetStateAction<ArtistEventType[]>>;
+  dispatch: React.Dispatch<ActionType>;
 }
 
 export const CollectionContext = createContext<null|contextProps>(null);
@@ -13,16 +19,37 @@ export const CollectionContext = createContext<null|contextProps>(null);
 
 type Props = {
   children: React.ReactNode
-  key: string
+  keys: string
 }
 
-export const CollectionContextProvider = ({children,key}: Props) => {
-  const [collection, setCollection] = useLocalStorage<ArtistEventType[] >({
-    key: key,
-    defaultValue: [] 
-  }); 
+
+function reducer(draft:ArtistEventType[],{action,keys: key,data}: ActionType) {
+  switch(action){
+    case 'add': {
+      draft.push(data)
+      localStorage.setItem(key,JSON.stringify(draft))
+      return draft
+    }
+    case 'remove': {
+      const i = draft.findIndex((item) => item.Booth_name === data.Booth_name)
+      draft.splice(i,1)
+      localStorage.setItem(key,JSON.stringify(draft))
+      return draft
+
+    }
+    default: {
+      throw new Error('Action not found in CollectionContext')
+    }
+      
+  }
+
+}
+
+export const CollectionContextProvider = ({children,keys}: Props) => {
+  const item = JSON.parse(localStorage.getItem(keys) || '[]')
+  const [collection, dispatch] =  useImmerReducer(reducer,item as ArtistEventType[])
   return (
-    <CollectionContext.Provider value={{collection,setCollection}}>{children}</CollectionContext.Provider>
+    <CollectionContext.Provider value={{collection,dispatch}}>{children}</CollectionContext.Provider>
   )
 }
 
--- src/context/CollectionContext/useCollectionContext.ts
+++ src/context/CollectionContext/useCollectionContext.ts
@@ -1,10 +1,11 @@
 import { useContext } from "react";
-import { CollectionContext } from "./CollectionContext";
+import { ActionType, CollectionContext } from "./CollectionContext";
+import { ArtistEventType } from "../../types/Artist";
 
-export function useCollectionProvider()  {
+export function useCollectionProvider(): [ArtistEventType[],React.Dispatch<ActionType>] {
   const data = useContext(CollectionContext);
   if(!data ) {
     throw new Error('useCollectionProvider must be used within CollectionProvider');
   }
-  return data;
+  return [data.collection,data.dispatch];
 }
