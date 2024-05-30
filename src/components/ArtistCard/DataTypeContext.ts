9f17260342c0c27466eaf898f639d17a51372448
Tan Kai <6572withspicy@gmail.com>
2024-05-30 22:42:14 +0800

    feat: Added DataTypeContext

---
+++ src/components/ArtistCard/DataTypeContext.ts
@@ -0,0 +1,10 @@
+import { createContext, useContext } from "react";
+
+type ArtistDataType = 'FF' | 'Artist' | 'Event';
+export const DataTypeContext = createContext<null|ArtistDataType>(null);
+
+export const useDataTypeContext = () => {
+  const context = useContext(DataTypeContext);
+  if(!context) throw new Error('useDataTypeContext must be used within DataTypeContext')
+  return context;
+}
