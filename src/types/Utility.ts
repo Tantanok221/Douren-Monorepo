e71e7cb814aa47c0e64126d4da569e0e591216cf
Tan Kai <6572withspicy@gmail.com>
2024-06-09 15:31:23 +0800

    feat: Added useState utility types

--- src/types/Utility.ts
+++ src/types/Utility.ts
@@ -1,1 +1,8 @@
+import React, { SetStateAction } from "react"
+
 export type Undefined<T> = T  | undefined
+
+export interface ReactUseState<T> {
+  state: T
+  setState: React.Dispatch<SetStateAction<T>>
+}
