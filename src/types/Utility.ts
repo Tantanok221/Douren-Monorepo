980a25cbb6aaa86c718db05e291c179ba955f6ba
Tan Kai <6572withspicy@gmail.com>
2024-06-09 15:46:49 +0800

    feat: Update ReactUseState Utility Types

--- src/types/Utility.ts
+++ src/types/Utility.ts
@@ -2,7 +2,7 @@ import React, { SetStateAction } from "react"
 
 export type Undefined<T> = T  | undefined
 
-export interface ReactUseState<T> {
-  state: T
-  setState: React.Dispatch<SetStateAction<T>>
-}
+export type ReactUseState<T> = [
+  state: T,
+  setState: React.Dispatch<React.SetStateAction<T>>
+]
