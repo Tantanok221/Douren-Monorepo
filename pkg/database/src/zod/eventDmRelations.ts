8787f236d62ebf2434eaa3202a740fb0e9e0b3f4
Tan Kai <6572withspicy@gmail.com>
2024-09-15 19:31:52 +0800

    fix: Remove useless zod schemas

--- pkg/database/src/zod/eventDmRelations.ts
+++
@@ -1,9 +0,0 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
-import { s } from "../db/index.js";
-
-export const eventDmRelationsInsertSchema = createInsertSchema(s.eventDmRelations);
-export const eventDmRelationsSelectSchema = createSelectSchema(s.eventDmRelations);
-
-export type zodEventDmRelationsInsertSchema = z.infer<typeof eventDmRelationsInsertSchema>;
-export type zodEventDmRelationsSelectSchema = z.infer<typeof eventDmRelationsSelectSchema>;
--- pkg/database/src/zod/eventRelations.ts
+++
@@ -1,9 +0,0 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
-import { s } from "../db/index.js";
-
-export const eventRelationsInsertSchema = createInsertSchema(s.eventRelations);
-export const eventRelationsSelectSchema = createSelectSchema(s.eventRelations);
-
-export type zodEventRelationsInsertSchema = z.infer<typeof eventRelationsInsertSchema>;
-export type zodEventRelationsSelectSchema = z.infer<typeof eventRelationsSelectSchema>;
