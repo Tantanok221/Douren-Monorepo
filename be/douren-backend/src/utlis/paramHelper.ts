25db5232f7236ef401347c7705e8551cabfa3c0f
Tan Kai <6572withspicy@gmail.com>
2024-09-16 10:28:06 +0800

    refactor: refactor query param parts

---
+++ be/douren-backend/src/utlis/paramHelper.ts
@@ -0,0 +1,25 @@
+import {processTableName} from "../helper/processTableName";
+import {AnyColumn, asc, desc, SQLWrapper} from "drizzle-orm";
+import {processTagConditions} from "../helper/processTagConditions";
+
+export function processArtistEventParams(sort: string, searchtable:string,tag:string): ArtistEventParams{
+    const table = processTableName(sort.split(",")[0]);
+    const sortBy = sort.split(",")[1] === "asc" ? asc : desc;
+    const searchTable = processTableName(searchtable);
+    const tagConditions = processTagConditions(tag);
+    return {
+        table,
+        sortBy,
+        searchTable,
+        tagConditions,
+        tag
+    }
+}
+
+export interface ArtistEventParams{
+    table: AnyColumn | SQLWrapper,
+    sortBy: typeof asc | typeof desc,
+    searchTable: AnyColumn
+    tagConditions:  SQLWrapper[]
+    tag: string
+}
