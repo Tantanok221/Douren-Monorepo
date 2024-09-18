539cfa5da5d9427e06bb30f55eb4a8114742cac5
Tan Kai <6572withspicy@gmail.com>
2024-09-18 20:54:18 +0800

    feat: Added trpc for fetching artist and event route

--- be/douren-backend/src/index.ts
+++ be/douren-backend/src/index.ts
@@ -1,20 +1,33 @@
 import { Env, Hono } from "hono";
 import { logger } from "hono/logger";
 import { initDB, up } from "@pkg/database/db";
-import { ENV_VARIABLE } from "./helper/constant";
 import { clerkMiddleware } from "@hono/clerk-auth";
 import { trimTrailingSlash } from "hono/trailing-slash";
-import ArtistRoute from "./routes/artist";
-import EventRoute from "./routes/event";
-
-const app = new Hono<{ Bindings: ENV_VARIABLE }>();
+import ArtistRoute, {trpcArtistRoute} from "./routes/artist";
+import EventRoute, {trpcEventRoute} from "./routes/event";
+import {router} from "./trpc";
+import { trpcServer } from '@hono/trpc-server'
+import {BACKEND_BINDING} from "@pkg/env/constant";
+const app = new Hono<{ Bindings: BACKEND_BINDING }>();
 app.use("*", clerkMiddleware());
 app.use("*", logger());
 app.use("*", trimTrailingSlash());
+
+const appRouter = router({
+  artist: trpcArtistRoute,
+  eventArtist: trpcEventRoute
+})
+export type AppRouter = typeof appRouter;
+
+app.use(
+    '/trpc/*',
+    trpcServer({
+      router: appRouter,
+    })
+)
 const routes = app
   .route("/event", EventRoute)
   .route("/artist", ArtistRoute)
-export type DourenBackend = typeof routes;
 
 export default {
   /** this part manages cronjobs */
---
+++ be/douren-backend/src/utlis/fetchHelper.ts
@@ -0,0 +1,57 @@
+import {cacheJsonResults, initRedis} from "@pkg/redis/redis";
+import {processArtistEventParams} from "./paramHelper";
+import {GetArtistQuery, GetEventArtistQuery} from "./queryHelper";
+import {createPaginationObject} from "../helper/createPaginationObject";
+import {PAGE_SIZE} from "../helper/constant";
+import {artistSchemaType, eventArtistSchemaType} from "../model/customObject";
+
+
+export async function ArtistFetchFunction(page: string, search: string, sort: string, searchtable:string, tag: string){
+    const redis = initRedis();
+    const ArtistParam = processArtistEventParams(sort, searchtable, tag)
+    const redisKey = `get_artist_${page}_${search}_${tag}_${sort}_${searchtable}`;
+    const redisData = await redis.json.get(redisKey, {}, "$");
+    if (redisData) {
+        console.log("redis cache hit");
+        return redisData as artistSchemaType;
+    }
+    const { SelectQuery,CountQuery} = GetArtistQuery(search,page,ArtistParam)
+    // TODO: Need to change front end to use, to split
+    const [data, [counts]] = await Promise.all([
+        SelectQuery.query,
+        CountQuery.query,
+    ]);
+    const returnObj = createPaginationObject(
+        data,
+        Number(page),
+        PAGE_SIZE,
+        counts.totalCount
+    ) as object;
+    console.log("Setting redis cache");
+    await cacheJsonResults(redis, redisKey, returnObj);
+    return returnObj as artistSchemaType
+}
+
+export async function EventArtistFetchFunction(page:string,search:string,sort:string,searchtable:string,tag:string,eventId:string){
+    const redis = initRedis();
+    const redisKey = `get_eventArtist${eventId}_${page}_${search}_${tag}_${sort}_${searchtable}`;
+    const redisData = await redis.json.get(redisKey, {}, "$");
+    if (redisData) {
+        console.log("redis cache hit");
+        return redisData as eventArtistSchemaType;
+    }
+    const artistEventParams = processArtistEventParams(sort, searchtable, tag)
+    const {SelectQuery, CountQuery} = GetEventArtistQuery(eventId, search, page, artistEventParams)
+    // TODO: Need to change front end to use(,) to split
+    const data = await SelectQuery.query;
+    const [counts] = await CountQuery.query;
+    const returnObj = createPaginationObject(
+        data,
+        Number(page),
+        PAGE_SIZE,
+        counts.totalCount,
+    ) as {};
+    console.log("Setting redis cache");
+    await cacheJsonResults(redis, redisKey, returnObj);
+    return returnObj as eventArtistSchemaType
+}
