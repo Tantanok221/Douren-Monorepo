19b95e581f30b3c5f161f9a26d18d70019958d94
Tan Kai <6572withspicy@gmail.com>
2024-10-03 00:40:16 +0800

    [#19] test: Added mocking data process

--- be/douren-backend/src/routes/event.test.ts
+++ be/douren-backend/src/routes/event.test.ts
@@ -1,15 +1,36 @@
-import {describe, expect, test} from "vitest";
+import {afterAll, beforeAll, describe, expect, test} from "vitest";
 import {app} from "../index";
 
 const MOCK_ENV = {
   DEV_ENV: "dev"
 };
 
+let artistId: string
+beforeAll(async () => {
+  const res = await app.request("/artist", {
+    method: "POST",
+    body: JSON.stringify({
+      "author": "dummy2"
+    }),
+    headers: new Headers({'Content-Type': 'application/json'}),
+  }, MOCK_ENV)
+  const data:any = await res.json()
+  artistId = data[0].uuid
+})
+
+afterAll(async () => {
+  const res = await app.request(`/artist/${artistId}`, {
+    method: "DELETE"
+  }, MOCK_ENV)
+  expect(res.status).toBe(200)
+  const data = await res.json()
+})
+
 describe("Test Event Routes", () => {
   test("GET /event/:eventId/artist", async () => {
     const res = await app.request("/event/2/artist?sort=Author_Main(Author),asc&page=1&searchtable=Author_Main.Author&tag=原創");
     expect(res.status).toBe(200);
-    let resJson:any = await res.json();
+    let resJson: any = await res.json();
     const payload = resJson.data
     expect(payload.length).toBeLessThanOrEqual(40)
   });
@@ -19,41 +40,39 @@ describe("Test Event Routes", () => {
         method: "POST",
         body: JSON.stringify({
           "boothName": "dummy2",
-          "artistId": 1279,
+          "artistId": artistId,
           "eventId": 2
         }),
         headers: new Headers({'Content-Type': 'application/json'}),
       }, MOCK_ENV)
       expect(res.status).toBe(201)
-      const resJson:any = await res.json()
+      const resJson: any = await res.json()
       createdId = resJson[0].uuid
-    console.log(resJson[0])
+      artistId = resJson[0].artistId
     }
   )
   test.sequential("PUT /event/artist", async () => {
-    console.log(createdId)
     const res = await app.request(`/event/artist`, {
       method: "PUT",
       body: JSON.stringify({
         "boothName": "dummy3",
         "uuid": createdId,
-        "artistId": 1279,
+        "artistId": artistId,
         "eventId": 2
       }),
       headers: new Headers({'Content-Type': 'application/json'}),
     }, MOCK_ENV)
     expect(res.status).toBe(200)
-    const resJson:any = await res.json()
+    const resJson: any = await res.json()
     expect(resJson[0].boothName).toBe("dummy3")
 
   })
   test.sequential("DELETE /event/:eventId/artist/:artistId", async () => {
-    const res = await app.request(`/event/2/artist/${createdId}`, {
+    const res = await app.request(`/event/2/artist/${artistId}`, {
       method: "DELETE"
     }, MOCK_ENV)
     expect(res.status).toBe(200)
     const data = await res.json()
-    console.log(data)
     return
   })
 })
