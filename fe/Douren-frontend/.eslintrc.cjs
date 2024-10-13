6cc57bade85bd13d75347a9312209a7431d6a9f2
Tan Kai <6572withspicy@gmail.com>
2024-10-13 18:53:43 +0800

    [#41] dev: Added linting to pre-push hooks

--- fe/Douren-frontend/.eslintrc.cjs
+++ fe/Douren-frontend/.eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
     "plugin:@typescript-eslint/strict",
     "prettier",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "template", "routeTree.gen.ts"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   parser: "@typescript-eslint/parser",
@@ -24,5 +24,6 @@ module.exports = {
     "react-hooks/exhaustive-deps": 0,
     "no-empty-pattern": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off"
   },
 };
