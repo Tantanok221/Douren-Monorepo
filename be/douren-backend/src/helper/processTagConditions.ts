4ab1293e25ce5ecf347b4f3c61715bb033dbf6f1
Tan Kai <6572withspicy@gmail.com>
2024-10-13 19:25:00 +0800

    [#41] fix: lint issue on douren backend

--- be/douren-backend/src/helper/processTagConditions.ts
+++ be/douren-backend/src/helper/processTagConditions.ts
@@ -4,8 +4,8 @@ import { ilike, SQLWrapper } from "drizzle-orm";
 export function processTagConditions(tag?: string) {
 	const tagConditions: SQLWrapper[] = [];
 	if (tag && tag.length > 0) {
-		for( const element of tag.split(",") ){
-			tagConditions.push(ilike(s.authorMain.tags, `%${element}%`))
+		for (const element of tag.split(",")) {
+			tagConditions.push(ilike(s.authorMain.tags, `%${element}%`));
 		}
 	}
 	return tagConditions;
