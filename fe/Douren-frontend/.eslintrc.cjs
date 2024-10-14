28cd83821891ed6fd6958df125b64ec477d10bea
Tan Kai <6572withspicy@gmail.com>
2024-10-14 22:10:12 +0800

    [DR-2] dev: Update linting rule

--- fe/Douren-frontend/.eslintrc.cjs
+++ fe/Douren-frontend/.eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-unused-vars": "off",
     "react-hooks/exhaustive-deps": 0,
     "no-empty-pattern": "off",
     "@typescript-eslint/no-explicit-any": "off",
