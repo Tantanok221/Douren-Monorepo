c71435a04c6d92202ca136812f5232d28e462f49
Tan Kai <6572withspicy@gmail.com>
2024-10-20 21:39:51 +0800

    [DR-54] chore: format

--- fe/Douren-frontend/src/routes/artist/index.tsx
+++ fe/Douren-frontend/src/routes/artist/index.tsx
@@ -3,10 +3,7 @@ import style from "./Artist.module.css";
 import classNames from "classnames/bind";
 import { usePageInit } from "@/hooks/usePageInit.ts";
 import { createFileRoute } from "@tanstack/react-router";
-import {
-  DataOperationProvider,
-  SearchBox,
-} from "@lib/ui";
+import { DataOperationProvider, SearchBox } from "@lib/ui";
 import { Animate } from "@/components";
 import { ArtistContainer } from "@/routes/artist/-components/ArtistContainer.tsx";
 import { FilterContainer } from "@/components/FilterContainer/";
@@ -16,7 +13,6 @@ const sortItem = [{ text: "排序: 作者名稱", value: "Author_Main(Author)" }
 const Artist = () => {
   usePageInit();
   const sx = classNames.bind(style);
-
 
   return (
     <DataOperationProvider>
