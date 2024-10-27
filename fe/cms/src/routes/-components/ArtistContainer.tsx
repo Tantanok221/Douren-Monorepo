8f93b6465e500662252928fe73dc1f00156c0781
Tan Kai <6572withspicy@gmail.com>
2024-10-27 12:39:25 +0800

    [DR-56] feat: Added sample code for artistcontainer

--- fe/cms/src/routes/-components/ArtistContainer.tsx
+++ fe/cms/src/routes/-components/ArtistContainer.tsx
@@ -1,6 +1,37 @@
 import { trpc } from "../../helper/trpc.ts";
+import { ArtistCard, useSearchContext, useSortSelectContext, useTagFilter } from "@lib/ui";
+import React, { useState } from "react";
 
 export const ArtistContainer = () => {
-
-  return <></>
+  const [search] = useSearchContext();
+  const [page, setPage] = useState(1);
+  const tagFilter = useTagFilter((state) => state.tagFilter);
+  const allTag = tagFilter.map((val) => val.tag).join(",");
+  const [sortSelect] = useSortSelectContext();
+  const res = trpc.artist.getArtist.useQuery({
+    search: search,
+    searchTable: "",
+    page: String(page),
+    sort: sortSelect,
+    tag: allTag,
+  });
+  if(!res.data) return null;
+  console.log(res.data)
+  return <>
+    {res.data.data?.map((item, index) => (
+      <ArtistCard key={index} data={item}>
+        <ArtistCard.ImageContainer />
+        <ArtistCard.RightContainer>
+          {/*<div className={sx("rightHeaderContainer")}>*/}
+            <div>
+            <ArtistCard.HeaderContainer />
+            <ArtistCard.TagContainer size="s" activeButton />
+          </div>
+          <ArtistCard.LinkContainer size="s">
+            {/*<ArtistCard.Button size="s" />*/}
+          </ArtistCard.LinkContainer>
+        </ArtistCard.RightContainer>
+      </ArtistCard>
+    ))}
+  </>
 }
