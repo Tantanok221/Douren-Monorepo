578fc5bff9fa839e0d65b79bca224bc171e8c54a
Tan Kai <6572withspicy@gmail.com>
2024-12-24 08:59:10 +0800

    [DR-60] feat: added empty artist route

---
+++ fe/cms/src/routes/form/product.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/form/product')({
+  component: () => <AllProductForm/>,
+})
+
+function AllProductForm(){
+  return (
+    <>
+    </>
+  )
+}
+
+function ProductForm(){
+
+}
