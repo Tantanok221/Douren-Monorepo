3a1bf8380748068381725bb1bc018fc58c66c2d1
Tan Kai <6572withspicy@gmail.com>
2025-01-01 16:42:08 +0800

    [DR-69] dev: log db

--- pkg/database/src/db/initdb.ts
+++ pkg/database/src/db/initdb.ts
@@ -4,6 +4,7 @@ import * as schema from "./schema.js";
 import { config } from "dotenv";
 export function initDB() {
   config();
+  console.log("db url: " + process.env.DATABASE_URL)
   const client = postgres(process.env.DATABASE_URL!);
   const db = drizzle(client, { schema });
   return db;
