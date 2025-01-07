42aa5ef5fb0fdd02ac3ca8fcf359ca2247d34656
Tan Kai <6572withspicy@gmail.com>
2025-01-07 23:19:34 +0800

    [DR-85] refactor: remove try catch and format

--- be/douren-backend/src/Dao/Tag.ts
+++ be/douren-backend/src/Dao/Tag.ts
@@ -1,12 +1,10 @@
-import {  initRedis } from "@pkg/redis/redis";
+import { initRedis } from "@pkg/redis/redis";
 import { initDB, s } from "@pkg/database/db";
 
-const tagKey = "Douren_Tag";
 
 export async function fetchTag(
 	db: ReturnType<typeof initDB>,
 	redis: ReturnType<typeof initRedis>,
 ) {
-	const data = await db.select().from(s.tag);
-	return data;
+	return db.select().from(s.tag);
 }
