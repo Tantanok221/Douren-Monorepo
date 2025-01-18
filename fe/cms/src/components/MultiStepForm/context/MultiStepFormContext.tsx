cb494e7c6204129df98db204c24f56b3de9f89ac
Tan Kai <6572withspicy@gmail.com>
2025-01-18 22:44:56 +0800

    [DR-68] refactor: extract MultiStepFormProvider function methods

--- fe/cms/src/components/MultiStepForm/context/MultiStepFormContext.tsx
+++ fe/cms/src/components/MultiStepForm/context/MultiStepFormContext.tsx
@@ -1,9 +1,9 @@
 import { MultiStepFormContext } from "./useMultiStepFormContext.ts";
 import { useState } from "react";
-import { createStore } from "zustand";
+import { createStore, StateCreator } from "zustand";
 import {
   ArtistFormSchema,
-  EventArtistSchema,
+  EventArtistSchema
 } from "@/routes/form/-components/form/schema";
 import { ProductFormSchema } from "../../../routes/form/-components/form/schema";
 
@@ -28,48 +28,50 @@ export interface MultiStepStore {
   triggerSubmit: () => void;
 }
 
+const multiStepFormFunction: StateCreator<MultiStepStore> = (set, get) => ({
+  step: 1,
+  submitState: "",
+  artistStep: null,
+  eventArtistStep: null,
+  productStep: null,
+  setArtistStep: (step) =>
+    set(() => ({
+      artistStep: step
+    })),
+  setEventArtistStep: (step) =>
+    set(() => ({
+      eventArtistStep: step
+    })),
+  setProductStep: (step) =>
+    set(() => ({
+      productStep: step
+    })),
+  bumpStep: () => {
+    set((state) => ({
+      step: state.step + 1
+    }));
+  },
+  goBackStep: () => {
+    set((state) => ({
+      step: state.step - 1
+    }));
+  },
+  setSubmitState: (state: submitStepState) => {
+    set(() => ({
+      submitState: state
+    }));
+  },
+  triggerSubmit: () => {
+    const artistData = get().artistStep;
+    const eventArtistData = get().eventArtistStep;
+    console.log("artist data:", artistData);
+    console.log("event artist data:", eventArtistData);
+  }
+});
+
 export const MultiStepFormProvider = ({ children }: props) => {
   const [store] = useState(() =>
-    createStore<MultiStepStore>((set, get) => ({
-      step: 1,
-      submitState: "",
-      artistStep: null,
-      eventArtistStep: null,
-      productStep: null,
-      setArtistStep: (step) =>
-        set(() => ({
-          artistStep: step,
-        })),
-      setEventArtistStep: (step) =>
-        set(() => ({
-          eventArtistStep: step,
-        })),
-      setProductStep: (step) =>
-        set(() => ({
-          productStep: step,
-        })),
-      bumpStep: () => {
-        set((state) => ({
-          step: state.step + 1,
-        }));
-      },
-      goBackStep: () => {
-        set((state) => ({
-          step: state.step - 1,
-        }));
-      },
-      setSubmitState: (state: submitStepState) => {
-        set(() => ({
-          submitState: state,
-        }));
-      },
-      triggerSubmit: () => {
-        const artistData = get().artistStep;
-        const eventArtistData = get().eventArtistStep;
-        console.log("artist data:", artistData);
-        console.log("event artist data:", eventArtistData);
-      },
-    })),
+    createStore<MultiStepStore>(multiStepFormFunction)
   );
 
   return (
