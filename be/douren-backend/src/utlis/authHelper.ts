856d6022ef9dd1f3ae06668ed543bbb107339437
Tan Kai <6572withspicy@gmail.com>
2025-01-18 23:33:54 +0800

    [DR-74] dev: format

--- be/douren-backend/src/utlis/authHelper.ts
+++ be/douren-backend/src/utlis/authHelper.ts
@@ -1,7 +1,7 @@
 import { Context } from "hono";
 
 export function verifyUser(c: Context): boolean {
-	const auth =	c.req.header("Authorization")?.split(" ")[1]
+	const auth = c.req.header("Authorization")?.split(" ")[1];
 	if (c.env.DEV_ENV === "dev") return true;
 	if (c.env.basic_auth_token === c.req.header("Authorization")) return true;
 	return false;
