5a0bc3ff01720f30693f0c6e33658730c58c3f50
Tan Kai <6572withspicy@gmail.com>
2025-01-18 23:31:39 +0800

    [DR-74] feat: basic auth for protected endpoint

--- be/douren-backend/src/trpc/index.ts
+++ be/douren-backend/src/trpc/index.ts
@@ -1,13 +1,20 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { BACKEND_BINDING } from "@pkg/env/constant";
 import { HonoVariables } from "@/index";
+import { verifyUser } from "@/utlis/authHelper";
+import { Context } from "hono";
 
 type HonoContext = {
 	env: BACKEND_BINDING;
+	honoContext: Context;
 } & HonoVariables;
 
 const t = initTRPC.context<HonoContext>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
-export const authProcedure = t.procedure;
+export const authProcedure = t.procedure.use(async (opts) => {
+	const {ctx} = opts;
+	if (!verifyUser(ctx.honoContext)) throw new TRPCError({code: "UNAUTHORIZED"})
+	return opts.next()
+});
