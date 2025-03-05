cd1e263fd61f19e7a1799ac6c7d0e314acd956f3
Tan Kai <6572withspicy@gmail.com>
2025-03-05 17:35:01 +0800

    [DR-74] feat: added tag helper

---
+++ lib/ui/src/helper/tag.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { ZodTagObject } from "../stores";
+
+const ZodTagArray = z.array(ZodTagObject);
+type ZodTagArrayType = z.infer<typeof ZodTagArray>;
+export class ArrayTagHelper {
+  tags: ZodTagArrayType;
+  constructor(tag: ZodTagArrayType) {
+    this.tags = tag;
+  }
+
+  toString() {
+    return this.tags.map((x) => x.tag).join(",");
+  }
+}
