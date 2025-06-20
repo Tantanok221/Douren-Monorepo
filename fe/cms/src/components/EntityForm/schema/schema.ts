1326f392fb36b02cfd2176384a1b4aa4f1ee1995
Tantanok221 <6572withspicy@gmail.com>
2025-06-20 15:45:16 +0800

    [DR-90] dev: format

--- fe/cms/src/components/EntityForm/schema/schema.ts
+++ fe/cms/src/components/EntityForm/schema/schema.ts
@@ -1,34 +1,34 @@
 import { z } from "zod";
 import { ZodTagObject } from "@lib/ui";
-import {LinkFormSchema} from "../../RichForm";
+import { LinkFormSchema } from "../../RichForm";
 
 export const productFormSchema = z.object({
-    title: z.string().min(1, { message: "請輸入作品名字" }),
-    preview: z.string().optional(),
-    thumbnail: z.string().optional(),
+  title: z.string().min(1, { message: "請輸入作品名字" }),
+  preview: z.string().optional(),
+  thumbnail: z.string().optional(),
 });
 
 export type ProductFormSchema = z.infer<typeof productFormSchema>;
 
 export const eventArtistSchema = z.object({
-    eventId: z.number().min(1, "請選擇一個活動"),
-    artistId: z.number(),
-    boothName: z.string().min(1, "請輸入攤位名字"),
-    dm: z.string().optional(),
-    locationDay01: z.string().optional(),
-    locationDay02: z.string().optional(),
-    locationDay03: z.string().optional(),
+  eventId: z.number().min(1, "請選擇一個活動"),
+  artistId: z.number(),
+  boothName: z.string().min(1, "請輸入攤位名字"),
+  dm: z.string().optional(),
+  locationDay01: z.string().optional(),
+  locationDay02: z.string().optional(),
+  locationDay03: z.string().optional(),
 });
 
 export type EventArtistSchema = z.infer<typeof eventArtistSchema>;
 
 export const artistFormSchema = z
-    .object({
-        introduction: z.string().optional(),
-        author: z.string().min(1, { message: "請輸入名字" }),
-        tags: z.array(ZodTagObject),
-        photo: z.string().optional(),
-    })
-    .merge(LinkFormSchema);
+  .object({
+    introduction: z.string().optional(),
+    author: z.string().min(1, { message: "請輸入名字" }),
+    tags: z.array(ZodTagObject),
+    photo: z.string().optional(),
+  })
+  .merge(LinkFormSchema);
 
 export type ArtistFormSchema = z.infer<typeof artistFormSchema>;
