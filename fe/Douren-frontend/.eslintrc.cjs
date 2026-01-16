711e145210b3eb4bfa3ddb42920936bdeb47a0ea
Stangen <stangendeveloper@gmail.com>
2026-01-17 00:03:13 +0800

    [DR-00] fix: disable problematic eslint rule in doujinbooth

--- fe/Douren-frontend/.eslintrc.cjs
+++ fe/Douren-frontend/.eslintrc.cjs
@@ -21,6 +21,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
     "react-hooks/exhaustive-deps": 0,
     "no-empty-pattern": "off",
     "@typescript-eslint/no-explicit-any": "off",
