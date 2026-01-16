fee634946822fc59b7b4cb68be14c3ce1ed7102b
Stangen <stangendeveloper@gmail.com>
2026-01-16 22:31:51 +0800

    [DR-00] feat: updated style

--- be/douren-backend/src/helper/processTableName.ts
+++ be/douren-backend/src/helper/processTableName.ts
@@ -2,14 +2,14 @@ import { s } from "@pkg/database/db";
 import type { PgColumn } from "drizzle-orm/pg-core";
 
 const TABLE_NAME_MAP: Record<string, PgColumn> = {
-  "Author_Main(Author)": s.authorMain.author,
-  "Author_Main.Author": s.authorMain.author,
-  Booth_name: s.eventDm.boothName,
-  Location_Day01: s.eventDm.locationDay01,
-  Location_Day02: s.eventDm.locationDay02,
-  Location_Day03: s.eventDm.locationDay03,
+	"Author_Main(Author)": s.authorMain.author,
+	"Author_Main.Author": s.authorMain.author,
+	Booth_name: s.eventDm.boothName,
+	Location_Day01: s.eventDm.locationDay01,
+	Location_Day02: s.eventDm.locationDay02,
+	Location_Day03: s.eventDm.locationDay03,
 };
 
 export function processTableName(table: string) {
-  return TABLE_NAME_MAP[table] ?? s.authorMain.author;
+	return TABLE_NAME_MAP[table] ?? s.authorMain.author;
 }
