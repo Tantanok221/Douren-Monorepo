29e8812757fb291e3daaedca7039ae20ab38bfcb
Stangen <stangendeveloper@gmail.com>
2026-01-16 22:30:05 +0800

    [DR-00] feat: updated some imports

--- be/douren-backend/src/helper/processTableName.ts
+++ be/douren-backend/src/helper/processTableName.ts
@@ -1,17 +1,18 @@
 import { s } from "@pkg/database/db";
+import type { PgColumn } from "drizzle-orm/pg-core";
 
 // Mapping of table name strings to database columns
-const TABLE_NAME_MAP: Record<string, ReturnType<typeof s.authorMain.author>> = {
-	"Author_Main(Author)": s.authorMain.author,
-	"Author_Main.Author": s.authorMain.author,
-	Booth_name: s.eventDm.boothName,
-	Location_Day01: s.eventDm.locationDay01,
-	Location_Day02: s.eventDm.locationDay02,
-	Location_Day03: s.eventDm.locationDay03,
+const TABLE_NAME_MAP: Record<string, PgColumn> = {
+  "Author_Main(Author)": s.authorMain.author,
+  "Author_Main.Author": s.authorMain.author,
+  Booth_name: s.eventDm.boothName,
+  Location_Day01: s.eventDm.locationDay01,
+  Location_Day02: s.eventDm.locationDay02,
+  Location_Day03: s.eventDm.locationDay03,
 };
 
 export function processTableName(table: string) {
-	// sort = "table_name,asc"
-	// The front is the table name and then the sort order
-	return TABLE_NAME_MAP[table] ?? s.authorMain.author;
+  // sort = "table_name,asc"
+  // The front is the table name and then the sort order
+  return TABLE_NAME_MAP[table] ?? s.authorMain.author;
 }
