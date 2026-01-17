9bcfe0edd624a3a4e2938ed7acfb6a4214b78b52
Stangen <stangendeveloper@gmail.com>
2026-01-17 17:17:54 +0800

    [DR-00] fix: make CMS build config consistent

--- fe/cms/vite.config.ts
+++ fe/cms/vite.config.ts
@@ -1,27 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    TanStackRouterVite()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react/jsx-runtime'],
-    exclude: [],
-  },
-  server: {
-    port: 5174
-  }
-})
+	plugins: [react(), TanStackRouterVite()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+		},
+	},
+	optimizeDeps: {
+		include: ["react", "react-dom", "react/jsx-runtime"],
+		exclude: [],
+	},
+	server: {
+		port: 5174,
+	},
+});
