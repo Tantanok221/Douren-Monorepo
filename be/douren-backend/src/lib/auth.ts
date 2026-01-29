eb4c344fb59a2a31df175a1a9816471d50aac3ee
Stangen <stangendeveloper@gmail.com>
2026-01-29 23:58:16 +0800

    [DR-00] chore: format auth hook

--- be/douren-backend/src/lib/auth.ts
+++ be/douren-backend/src/lib/auth.ts
@@ -158,13 +158,13 @@ export const auth = (env: ENV_BINDING) => {
 							// We'll use a WeakMap or similar if we could, but here we'll try attaching to ctx.
 							setInviteContext(ctx, inviteCode, validation);
 
-						// Don't save inviteCode to the user table
-						const userWithInvite = user as typeof user & {
-							inviteCode?: unknown;
-						};
-						const { inviteCode, ...userData } = userWithInvite;
-						void inviteCode;
-						return { data: userData };
+							// Don't save inviteCode to the user table
+							const userWithInvite = user as typeof user & {
+								inviteCode?: unknown;
+							};
+							const { inviteCode, ...userData } = userWithInvite;
+							void inviteCode;
+							return { data: userData };
 						} catch (e) {
 							if (e instanceof APIError) throw e;
 							console.error("Error in before hook:", e);
